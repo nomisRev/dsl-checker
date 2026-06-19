d5f3b400eb07e9297f1419f16102d739c8504177
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-06-19 09:27:30 +0200

    Initial commit

---
+++ compiler-plugin/test-fixtures/org/jetbrains/kotlin/compiler/plugin/template/GenerateTests.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.kotlin.compiler.plugin.template
+
+import org.jetbrains.kotlin.compiler.plugin.template.runners.AbstractJsBoxTest
+import org.jetbrains.kotlin.compiler.plugin.template.runners.AbstractJvmBoxTest
+import org.jetbrains.kotlin.compiler.plugin.template.runners.AbstractJvmDiagnosticTest
+import org.jetbrains.kotlin.generators.dsl.junit5.generateTestGroupSuiteWithJUnit5
+
+fun main() {
+    generateTestGroupSuiteWithJUnit5 {
+        testGroup(testDataRoot = "compiler-plugin/testData", testsRoot = "compiler-plugin/test-gen") {
+            testClass<AbstractJvmDiagnosticTest> {
+                model("diagnostics")
+            }
+
+            testClass<AbstractJvmBoxTest> {
+                model("box")
+            }
+
+            testClass<AbstractJsBoxTest> {
+                model("box")
+            }
+        }
+    }
+}
